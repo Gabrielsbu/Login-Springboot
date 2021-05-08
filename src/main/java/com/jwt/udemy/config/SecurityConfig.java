76344b180f81e34a4dc469169fd3e72e8f7414c3
gabrielsbu <gabrielmaiasbu@gmail.com>
2021-05-08 20:53:49 -0300

    Introducao ao json jwt

--- src/main/java/com/jwt/udemy/config/SecurityConfig.java
+++ src/main/java/com/jwt/udemy/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,6 +51,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .hasAnyRole("USER", "ADMIN")
                 .antMatchers("/produtos/**")
                     .hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/usuarios/**")
+                    .permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic();
     }
